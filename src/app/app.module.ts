6f0a9395c1ce4305a38571b8d5cff8da6c94743f
ahajri <anis.hajri@conseil-etat.fr>
2019-05-31 16:59:57 +0200

    Init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GeoMapComponent } from './geo-map/geo-map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GeoMapComponent
   ],
   imports: [
     BrowserModule,
